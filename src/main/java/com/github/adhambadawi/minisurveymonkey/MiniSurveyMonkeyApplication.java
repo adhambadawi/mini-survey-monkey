32fed7e9c4b3d79ba2421eb9eeef8d391283585f
Adham Badawi <adhambadawi@cmail.carleton.ca>
2024-11-09 10:01:53 -0500

    Add main application class

---
+++ src/main/java/com/github/adhambadawi/minisurveymonkey/MiniSurveyMonkeyApplication.java
@@ -0,0 +1,12 @@
+package com.github.adhambadawi.minisurveymonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSurveyMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSurveyMonkeyApplication.class, args);
+    }
+}
