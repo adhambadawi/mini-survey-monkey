a4cb9de3297fe920bcb183ae396e0459e42676d2
Jaden Sutton <jadensutton_3@hotmail.com>
2024-11-10 12:53:25 -0500

    Added functionality to fill out surveys

--- src/main/java/com/github/adhambadawi/minisurveymonkey/controller/ApplicationController.java
+++ src/main/java/com/github/adhambadawi/minisurveymonkey/controller/ApplicationController.java
@@ -51,5 +51,8 @@ public class ApplicationController {
         return "index";
     }
 
-
+    @GetMapping("/survey/fill")
+    public String fillSurvey() {
+        return "fillSurvey";
+    }
 }
--- src/main/java/com/github/adhambadawi/minisurveymonkey/repository/ResponseRepository.java
+++ src/main/java/com/github/adhambadawi/minisurveymonkey/repository/ResponseRepository.java
@@ -1,17 +1,15 @@
 package com.github.adhambadawi.minisurveymonkey.repository;
 
-import com.github.adhambadawi.minisurveymonkey.model.Response;
 import com.github.adhambadawi.minisurveymonkey.model.Question;
+import com.github.adhambadawi.minisurveymonkey.model.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ResponseRepository extends JpaRepository<Response, Long> {
+    Optional<Response> findById(Long id);
     List<Response> findByQuestion(Question question);
-    List<Response> findByQuestionId(Long questionId);
-    List<Response> findByTextResponseContainingIgnoreCase(String text);
-    List<Response> findByNumberResponse(Integer number);
-    List<Response> findByChoiceResponse(String choice);
 }
